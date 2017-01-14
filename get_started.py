554809ced406c1e5b011e444953e298ab11aa95c
weiwei_cai_ <shiyybua@gmil.com>
2017-01-14 17:19:25 +0800

    method callable

--- get_started.py
+++ get_started.py
@@ -6,13 +6,14 @@ import urllib
 url_get_base = "http://api.ltp-cloud.com/analysis/"
 # 读API，除去结尾的\n符号。
 my_api = open('my_api').readline()[:-1]
+dafault_txt = '我是中国人。'
 
 
-def split():
+def split(str_txt = dafault_txt):
     print '- '* 20, 'ws 分词', '- ' * 20
     args = {
         'api_key': my_api,
-        'text': '我是中国人。',
+        'text': str_txt,
         'pattern': 'ws',
         'format': 'plain'
     }
@@ -22,12 +23,12 @@ def split():
 
 
 # 如果get_entity_list是False，那么则返回所有内容，反之则仅返回 命名实体列表
-def ner(get_entity_list = False):
+def ner(get_entity_list = False, str_txt=dafault_txt):
     print '- '* 20, '命名实体识别', '- ' * 20
     if get_entity_list:
         args = {
             'api_key': my_api,
-            'text': '我是中国人。',
+            'text': str_txt,
             'pattern': 'ner',
             'format': 'plain',
             'only_ner' : 'true'
@@ -46,11 +47,11 @@ def ner(get_entity_list = False):
 
 
 # 依存句法分析
-def dependency():
+def dependency(str_txt=dafault_txt):
     print '- ' * 20, '句法分析 dp', '- ' * 20
     args = {
         'api_key': my_api,
-        'text': '我是中国人。',
+        'text': str_txt,
         'pattern': 'dp',
         'format': 'plain'
     }
@@ -60,11 +61,11 @@ def dependency():
 
 
 # 语义依存分析
-def semantic_dependency():
+def semantic_dependency(str_txt=dafault_txt):
     print '- ' * 20, '语义依存分析 sdp', '- ' * 20
     args = {
         'api_key': my_api,
-        'text': '我是中国人。',
+        'text': str_txt,
         'pattern': 'sdp',
         'format': 'plain'
     }
@@ -74,11 +75,11 @@ def semantic_dependency():
 
 
 # 语义角色标注
-def role_tag():
+def role_tag(str_txt=dafault_txt):
     print '- ' * 20, '语义角色标注 srl', '- ' * 20
     args = {
         'api_key': my_api,
-        'text': '我是中国人。',
+        'text': str_txt,
         'pattern': 'srl',
         'format': 'plain'
     }
@@ -89,11 +90,11 @@ def role_tag():
 
 # 对文本进行全部任务的分析，返回xml
 # 具体每个节点标签的含义 见：http://www.ltp-cloud.com/document/
-def show_all_xml():
+def show_all_xml(str_txt=dafault_txt):
     print '- ' * 20, 'show all via xml', '- ' * 20
     args = {
         'api_key': my_api,
-        'text': '我是中国人。',
+        'text': str_txt,
         'pattern': 'all',
         'format': 'xml'
     }
@@ -104,11 +105,11 @@ def show_all_xml():
 
 # 对文本进行全部任务的分析，json
 # 通过添加has_key=false字段可以掉键值名
-def show_all_json():
+def show_all_json(str_txt=dafault_txt):
     print '- ' * 20, 'show all via json', '- ' * 20
     args = {
         'api_key': my_api,
-        'text': '我是中国人。',
+        'text': str_txt,
         'pattern': 'all',
         'format': 'json'
     }
@@ -118,12 +119,11 @@ def show_all_json():
 
 
 # 对文本进行全部任务的分析，用table表示出来
-# This method seems not working.
-def show_conll():
+def show_conll(str_txt=dafault_txt):
     print '- ' * 20, 'show all via table', '- ' * 20
     args = {
         'api_key': my_api,
-        'text': '我是中国人。',
+        'text': str_txt,
         'pattern': 'all',
         'format': 'conll'
     }
@@ -132,11 +132,11 @@ def show_conll():
     print content
 
 if __name__ == '__main__':
-    split()
+    # split()
     # ner(True)
     # dependency()
     # semantic_dependency()
-    # role_tag()租个车啥的
+    # role_tag()
     # show_all_xml()
     # show_all_json()
-    # show_conll()
+    show_conll()
